b4a326f76f1e14584ff6f4d17af08253c9e4315b
Steve Yu <74366156+kxdxzr@users.noreply.github.com>
2022-06-29 11:50:05 +0800

    Add files via upload

---
+++ BMET_2903_constants.m
@@ -0,0 +1,12 @@
+q = 1.60 * 10^-19; %C
+k = 8.988 * 10^9; %N*m^2/C^2, 9 * 10^9 for most calculation
+epslion0 = 8.85 * 10^-12; %C^2/N*m^2 
+mu0 = 1.257 * 10^-6; %Wb/A*m, T*m/A
+c = 3.0 * 10^8; %speed of light, m/s
+h = 6.62607015e-34; %planck's constant, J*s
+me = 9.10938356e-31; % mass of electron, kg
+mp = 1.672621898e-27; % mass of proton,kg
+R = 1.097e7;
+r_sun = 6.96e8;
+sigma = 5.67e-8;
+R0 = 1.2e-15;
